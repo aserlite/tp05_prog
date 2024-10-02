21128e157516743a7346c6fa9a044577a974ac1c
aserlite <arthurcuvillon691@gmail.com>
2024-10-02 09:06:37 +0200

    ex Bonus

--- src/exBonus.cpp
+++ src/exBonus.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
 int U(int n);
-
 int V(int n);
 
 int U(int n) {
     if (n == 0) return 1;
-    for (size_t i = 1; i < n; i++)
-    {
-        
-    }
-    
     return 3 * U(n - 1) + 2 * V(n - 1);
 }
 
 int V(int n) {
     if (n == 0) return 2;
-    return 2 * V(n - 1) + U(n);
+    return 2 * V(n - 1) + U(n - 1) + 1; 
 }
 
 int main() {
-    std::cout << "Les dix premiers termes de la suite U sont : ";
+    cout << "Les dix premiers termes de la suite U sont : ";
     for (int i = 0; i < 10; ++i) {
-        std::cout << U(i) << " ";
+        cout << U(i) << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    std::cout << "Les dix premiers termes de la suite V sont : ";
+    cout << "Les dix premiers termes de la suite V sont : ";
     for (int i = 0; i < 10; ++i) {
-        std::cout << V(i) << " ";
+        cout << V(i) << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
